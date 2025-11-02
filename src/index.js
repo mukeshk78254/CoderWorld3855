c583f7f50fcca7f05f0c64199098e14ff9916ea7
Mukesh Kumar <mukeshk78254@gmail.com>
2025-11-03 00:11:54 +0530

    update cors

--- src/index.js
+++ src/index.js
@@ -58,8 +58,8 @@ app.use(cors({
         'http://localhost:5173', 
         'http://localhost:5174', 
         'http://localhost:3000',
-        'https://coderworld3855.vercel.app',
-        'https://coderworld3855-5.onrender.com'
+        
+        'https://coder-world3855.vercel.app'
     ], 
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
