98c6052c22a9479ada9be144023749a5b92761d8
Mukesh Kumar <mukeshk78254@gmail.com>
2025-11-02 16:48:05 +0530

    Updated CORS to accept all Vercel deployment URLs

--- src/index.js
+++ src/index.js
@@ -55,11 +55,8 @@ const paymentRoutes = require('./routes/payment');
 
 app.use(cors({
     origin: [
-        'http://localhost:5173', 
-        'http://localhost:5174', 
-        'http://localhost:3000',
-        'https://coderworld3855.vercel.app',
-        'https://coderworld3855-5.onrender.com'
+        'http://localhost:5173' || process.env.CLIENT_URL , 
+        
     ], 
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
